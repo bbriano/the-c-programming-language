1fb80e2e95e3bed22925a0ac605cd88c8199072b
Briano Goestiawan <b@briano.io>
2020-11-20 20:08:57 +1100

    the unix system interface

---
+++ 8-the-unix-system-interface/file_descriptors.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("some output\n");
+    fprintf(stderr, "Error: bad things happened\n");
+}
---
+++ 8-the-unix-system-interface/syscall.c
@@ -0,0 +1,6 @@
+#include "syscalls.h"
+#define BUF_SIZE 100
+
+int main() {
+    char buf[BUF_SIZE];
+}
